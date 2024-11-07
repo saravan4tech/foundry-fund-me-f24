cf7c605ed83bed95f5952d61d38eb530fae2f6cf
saravan <saravan4tech@gmail.com>
2024-11-07 07:01:45 +0000

    first commit

--- test/unit/ZkSyncDevOps.t.sol
+++ test/unit/ZkSyncDevOps.t.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
+
 import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
 import {FoundryZkSyncChecker} from "lib/foundry-devops/src/FoundryZkSyncChecker.sol";
 
 contract ZkSyncDevOps is Test, ZkSyncChainChecker, FoundryZkSyncChecker {
     // Remove the `skipZkSync`, then run `forge test --mt testZkSyncChainFails --zksync` and this will fail!
-    function testZkSyncChainFails() public skipZkSync {
+    function testZkSyncChainFails() public  skipZkSync{
         address ripemd = address(uint160(3));
 
         bool success;
@@ -23,8 +24,8 @@ contract ZkSyncDevOps is Test, ZkSyncChainChecker, FoundryZkSyncChecker {
     // // Remove the `onlyVanillaFoundry`, then run `foundryup-zksync` and then
     // // `forge test --mt testZkSyncFoundryFails --zksync`
     // // and this will fail!
-    // function testZkSyncFoundryFails() public onlyVanillaFoundry {
-    //     bool exists = vm.keyExistsJson('{"hi": "true"}', ".hi");
-    //     assert(exists);
-    // }
-}
+    //function testZkSyncFoundryFails() public onlyVanillaFoundry {
+    //bool exists = vm.keyExistsJson('{"hi": "true"}', ".hi");
+    //assert(exists);
+    //}
+ }
